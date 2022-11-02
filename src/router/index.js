fc286635d36f06a5b5d467c350e17fe2f5a94dba
NguyenMinhKhanhB1909929 <khanhb1909929@student.ctu.edu.vn>
2022-11-03 01:57:33 +0700

    created add contactbook

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,16 @@ const routes = [
     component: () => import("@/views/NotFound.vue"),
   },
   {
+    path: "/contacts/add",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
-    props: true,
+    props: true, // Truyền các biến trong $route.params vào làm props
   },
 ];
 const router = createRouter({
